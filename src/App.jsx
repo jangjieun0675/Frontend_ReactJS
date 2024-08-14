77ff74d5a5e88af0ac65089ce98afa1355b2a01f
jieun <jangji0513@naver.com>
2024-08-14 10:25:28 +0900

    [MOD] : App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
+import "./App.css";
+import HeaderComponent from "src/components/common/HeaderComponent";
+import FooterComponent from "src/components/common/FooterComponent";
+
+function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <HeaderComponent />
+        <FooterComponent />
+      </BrowserRouter>
+    </>
   );
-};
+}
 
 export default App;
